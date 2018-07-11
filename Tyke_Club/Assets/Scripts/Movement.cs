c6382e252a72360ea8da526cf2d9f020479f21da
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-07-10 20:20:09 -0400

    Rotation of Camera is In

--- Tyke_Club/Assets/Scripts/Movement.cs
+++ Tyke_Club/Assets/Scripts/Movement.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-	public float moveSpeed; // Multiplier that stands for the Speed of the Player's movement
-	private Vector3 pos;
-	void Start () {
-
-	}
+	public float moveSpeed; // Multiplier that stands for the Speed of the Object's movement
+	private Vector3 pos; // Constructor to hold the current Position of this Object
 	
 	void Update () {
-		pos = gameObject.transform.position;
-		if(Input.GetButton("Horizontal")) {
-			pos.x += Input.GetAxis("Horizontal") * moveSpeed;
+		pos = gameObject.transform.position; // Updating the variable's value
+		if(Input.GetButton("Horizontal")) { // Whenever the A, D or Left, Right Keys are used it'll move either left or right
+			pos.x += Input.GetAxis("Horizontal") * moveSpeed; // Gets the desired x value
 		}
-		if(Input.GetButton("Vertical")) {
-			pos.z += Input.GetAxis("Vertical") * moveSpeed;
+		if(Input.GetButton("Vertical")) { // whenever the W, S or Up, Down Keys are used it'll move either Up or Down
+			pos.z += Input.GetAxis("Vertical") * moveSpeed; // GEts the desired z value
 		}
-		gameObject.transform.position = pos;
+		gameObject.transform.position = pos; // Updates the position of the Object with the desired values
 	}
 }
