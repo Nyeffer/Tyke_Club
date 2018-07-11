7666ecf147b7bf9f5d7caf8be5ff5afdbd89cc2e
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-07-10 20:04:42 -0400

    Set up Basic Folder and Include Movement

---
+++ Tyke_Club/Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float moveSpeed; // Multiplier that stands for the Speed of the Player's movement
+	private Vector3 pos;
+	void Start () {
+
+	}
+	
+	void Update () {
+		pos = gameObject.transform.position;
+		if(Input.GetButton("Horizontal")) {
+			pos.x += Input.GetAxis("Horizontal") * moveSpeed;
+		}
+		if(Input.GetButton("Vertical")) {
+			pos.z += Input.GetAxis("Vertical") * moveSpeed;
+		}
+		gameObject.transform.position = pos;
+	}
+}
