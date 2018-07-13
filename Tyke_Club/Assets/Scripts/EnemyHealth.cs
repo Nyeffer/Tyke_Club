1a23156cb6cf7ef0cdd8ef35d68c076dfb38200a
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-07-13 19:33:44 -0400

    Started On Enemy AI

---
+++ Tyke_Club/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public int maxHealth = 100;
+	public int curHealth;
+
+	private GameObject Player_bullet;
+    public GameObject player;
+
+	private Renderer renderer;
+
+	bool isDead;
+	bool isDamaged;
+
+	void Awake() {
+		renderer = GetComponent<Renderer>();
+		curHealth = maxHealth;
+		TakeDamage(0);
+	}
+
+	void Update() {
+		if(isDamaged) {
+			// Do Something
+		} else {
+			// Do Something else
+		} 
+		isDamaged = false;
+	}
+
+	public void TakeDamage(int damageDealt) {
+		isDamaged = true;
+		curHealth -= damageDealt;
+
+		if(curHealth <= 0 && !isDead) {
+			if(Random.RandomRange(0,2) == 1) {
+				GetComponent<Spawn>().enabled = true;
+				Death();
+			} else {
+				Death();
+			}
+		}
+	}
+
+	void Death() {
+        player.GetComponent<PlayerHealth>().AddGold(100);
+		GetComponent<CapsuleCollider>().enabled = false;
+		isDead = true;
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.tag == "PlayerObjects" && gameObject.tag == "Enemy") {
+			Player_bullet = col.gameObject;
+			TakeDamage(Player_bullet.GetComponent<BulletBehavior>().GetDamage());
+			Destroy(col.gameObject);
+		}
+ 	}
+
+	public bool GetisDead() {
+		return isDead;
+	}
+}
---
+++ Tyke_Club/Assets/Scripts/Player_Detection.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Detection : MonoBehaviour {
+	private GameObject target;
+	private Rigidbody rigidbody;
+	public Vector3 velocity;
+	public bool isSeeking;
+	public float moveSpeed;
+	private int numOfPoints;
+	float counter = 0;
+	float otherCounter = 0;
+	private bool isDamaging = false;
+	private Animator childAnim;
+	private bool child;
+	public GameObject weapon;
+
+	private float distanceToSeek;
+
+	void Awake() {
+		rigidbody = GetComponent<Rigidbody>();
+		velocity = rigidbody.velocity;
+		distanceToSeek = GetComponent<SphereCollider> ().radius;
+	}
+
+	void Start() {
+		childAnim = GetComponentInChildren<Animator>();
+		weapon.SetActive(false);
+	}
+
+	void Update() {
+		child = GetComponentInChildren<EnemyHealth>().GetisDead();
+		if(child) {
+			childAnim.SetBool("isDead", true);
+			
+			if(counter >= 10.0f) {
+				counter = 0;
+				Destroy(this.gameObject);
+			} else {
+				isSeeking = false;
+				counter += Time.deltaTime;
+			}
+		}
+	}
+
+	void FixedUpdate() {
+		if(isSeeking && !child) {
+			if(Vector3.Distance(target.transform.position, this.transform.position) < distanceToSeek + 5) {
+				Vector3 pos = target.transform.position - this.transform.position;
+				this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(pos), 0.1f);
+				if (!isDamaging) {
+					if (pos.magnitude > 2.5f) {
+						this.transform.Translate (0, 0, moveSpeed * Time.deltaTime);
+					} else {
+						isDamaging = true;
+						childAnim.SetBool("isAttacking", true);
+						weapon.SetActive(true);
+					} 
+				} else {
+					if (counter <= 0.5f) {
+						childAnim.SetBool("isAttacking", false);
+						counter = 0;
+						isDamaging = false;
+					} else {
+						this.transform.Translate (0, 0, (moveSpeed * 0) * Time.deltaTime);
+						counter += Time.deltaTime;
+					}
+				}
+			} 
+		}
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.tag == "Player") {
+			target = col.gameObject;
+			isSeeking = true;	
+		}
+	}
+
+	void OnTriggerStay(Collider col) {
+		if(col.gameObject.tag == "Player") {
+			target = col.gameObject;
+			isSeeking = true;	
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if (col.gameObject.tag == "Player") {
+			target = null;
+			isSeeking = false;
+		}
+ 	}
+}
