66c9069f758e2946b088a569ffbe21b759b4301c
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-08-14 20:31:47 -0400

    Model In

--- Tyke_Club/Assets/Scripts/Movement.cs
+++ Tyke_Club/Assets/Scripts/Movement.cs
@@ -6,24 +6,65 @@ public class Movement : MonoBehaviour {
 
 	public float moveSpeed; // Multiplier that stands for the Speed of the Object's movement
 	private Vector3 pos; // Constructor to hold the current Position of this Object
+	private Animator anim;
+
+	private bool isRunning;
+	private bool isRunning_Back;
+	private bool isStrafing_Left;
+	private bool isStrafing_Right;
+
+	void Start() {
+		anim = GetComponent<Animator>();
+		isRunning = false;
+		isRunning_Back = false;
+		isStrafing_Left = false;
+		isStrafing_Right = false;
+	}
 	
 	void Update () {
 		pos = gameObject.transform.position; // Updating the variable's value
 		if(Input.GetButton("Horizontal")) { // Whenever the A, D or Left, Right Keys are used it'll move either left or right
+			
 			if(Input.GetAxis("Horizontal") > 0) {
+				isStrafing_Left = false;
+				isStrafing_Right = true;
+				anim.SetBool("isStrafing_Left", isStrafing_Left);
+				anim.SetBool("isStrafing_Right", isStrafing_Right);
 				pos += transform.right * Time.deltaTime * moveSpeed; // Gets the desired x value
 			}
 			if(Input.GetAxis("Horizontal") < 0) {
+				isStrafing_Left = true;
+				isStrafing_Right = false;
+				anim.SetBool("isStrafing_Left", isStrafing_Left);
+				anim.SetBool("isStrafing_Right", isStrafing_Right);
 				pos -= transform.right * Time.deltaTime * moveSpeed; // Gets the desired x value
 			}
+		} else {
+			isStrafing_Left = false;
+			isStrafing_Right = false;
+			anim.SetBool("isStrafing_Left", isStrafing_Left);
+			anim.SetBool("isStrafing_Right", isStrafing_Right);
 		}
 		if(Input.GetButton("Vertical")) { // whenever the W, S or Up, Down Keys are used it'll move either Up or Down
 			if(Input.GetAxis("Vertical") > 0) {
+				isRunning = false;
+				isRunning_Back = true;
+				anim.SetBool("isRunning_Back", isRunning_Back);
+				anim.SetBool("isRunning", isRunning);
 				pos += transform.forward * Time.deltaTime * moveSpeed; // Gets the desired z value
 			}
 			if(Input.GetAxis("Vertical") < 0) {
+				isRunning = true;
+				isRunning_Back = false;
+				anim.SetBool("isRunning_Back", isRunning_Back);
+				anim.SetBool("isRunning", isRunning);
 				pos -= transform.forward * Time.deltaTime * moveSpeed; // Gets the desired z value
 			}
+		} else {
+			isRunning = false;
+			isRunning_Back = false;
+			anim.SetBool("isRunning_Back", isRunning_Back);
+			anim.SetBool("isRunning", isRunning);
 		}
 		transform.position = pos; // Updates the position of the Object with the desired values
 	}
