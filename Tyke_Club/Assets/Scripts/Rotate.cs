59db0b18995c49fe93d04702270beb4bf42e652a
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-08-22 21:12:41 -0400

    scorpoin in

---
+++ Tyke_Club/Assets/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+	public float xRotation = 90.0f;
+ 	public float xRotation1 = 0.0f;
+ 	public float RotationSpeed = 2.0f;
+	private float counter = 0.0f;
+ 
+ // Update is called once per frame
+	void Update () 
+	{
+		counter += Time.deltaTime;
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, counter * 10, transform.eulerAngles.z);
+		if(counter >= 360.0f) {
+			counter = 0;
+		}
+	}
+}
